9cb38427caa2ba1e3521f8736a54536eb458e032
김세한 <kmp1007s@naver.com>
2019-11-21 16:16:22 +0900

    Init Project
    Init Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Login from './Login.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <h1>App page</h1> */}
+      <Switch>
+        <Route path="/" component={Login} exact={true} />
+        <Route
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다:</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+      </Switch>
     </div>
   );
 }
